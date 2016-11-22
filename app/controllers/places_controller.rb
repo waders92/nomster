05e580a2a4042e7c5333104e7394aeabf68c8157
waders92 <wade.carlson@pga.com>
2016-11-22 12:09:14 -0600

    Added edit page and updated styles

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -3,8 +3,10 @@ before_action :authenticate_user!, only: [:new, :create]
   
   
   def index
-    @places = Place.paginate(page: params[:page], per_page: 5)
+    #@places = Place.paginate(page: params[:page], per_page: 5)
+    @places = Place.order("created_at ASC").all.paginate(page: params[:page], per_page:5)
   end
+  
 
   def new
     @place = Place.new
@@ -23,6 +25,11 @@ before_action :authenticate_user!, only: [:new, :create]
     @place = Place.find(params[:id])
   end
   
+  def update
+    @place = Place.find(params[:id])
+    @place.update_attributes(place_params)
+    redirect_to root_path
+  end
   
   private
   
