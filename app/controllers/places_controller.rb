044c84da3e0205aa94bdf64b862691f802f21420
waders92 <wade.carlson@pga.com>
2016-11-22 13:11:52 -0600

    Validation continued and added styling to it

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -13,8 +13,12 @@ before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destro
   end
   
   def create
-  current_user.places.create(place_params)
+  @place = current_user.places.create(place_params)
+    if @place.valid?
     redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
   
   def show
@@ -35,7 +39,11 @@ before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destro
     end
     
     @place.update_attributes(place_params)
-    redirect_to root_path
+    if @place.valid?
+      redirect_to root_path
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
   
   def destroy
