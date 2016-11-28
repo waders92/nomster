571c29a2ff09148c6c6d446f6d48cfc6e4e19915
waders92 <wade.carlson@pga.com>
2016-11-28 16:36:30 -0600

    fixed photo upload button, now error in submission

--- db/migrate/20161128162556_alter_photos.rb
+++ db/migrate/20161128162556_alter_photos.rb
@@ -1,5 +1,7 @@
 class AlterPhotos < ActiveRecord::Migration
   def change
     add_column :photos, :caption, :string
+    add_column :photos, :picture, :string
+    add_column :photos, :place_id, :integer
   end
 end
