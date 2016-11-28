059fa72c106f605c282c23c4943927ecf4b2a6e7
waders92 <wade.carlson@pga.com>
2016-11-28 17:02:52 -0600

    updated photos_controller

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,13 +1,15 @@
 class PhotosController < ApplicationController
   before_action :authenticate_user!
   def create
-    @place.photos.create(photo_params.merge(user: current_user))
+    @place = Place.find(params[:id])
+    
+    @place.photos.create(photo_params)
     redirect_to place_path(@place)
   end
   
   private
   
   def photo_params
-    params.require(:caption)
+    params.require(:photo).permit(:caption)
   end
 end
