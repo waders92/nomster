b5d9656490082e94141b8f2ddc881c0db48548c3
waders92 <wade.carlson@pga.com>
2016-11-28 12:38:51 -0600

    add photo button displays - model box doesn't show when clicked

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -2,7 +2,7 @@ class PhotosController < ApplicationController
   before_action :authenticate_user!
   def create
     @place = Place.find(params[:place_id])
-    @place.photos.create(comment_params.merge(user: current_user))
+    @place.photos.create(caption_params.merge(user: current_user))
     redirect_to place_path(@place)
   end
 end
