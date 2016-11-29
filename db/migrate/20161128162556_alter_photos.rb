82f6714c5d935112d10a230937d0299aa39459e1
waders92 <wade.carlson@pga.com>
2016-11-29 15:58:49 -0600

    cleaned up database migration

--- db/migrate/20161128162556_alter_photos.rb
+++ db/migrate/20161128162556_alter_photos.rb
@@ -1,7 +1,5 @@
 class AlterPhotos < ActiveRecord::Migration
   def change
-    add_column :photos, :caption, :string
     add_column :photos, :picture, :string
-    add_column :photos, :place_id, :integer
-  end
+   end
 end
