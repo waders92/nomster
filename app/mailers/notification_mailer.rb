bd81bc7b2280a6367a1ae95427c88f4227f4db78
waders92 <wade.carlson@pga.com>
2016-12-01 12:44:04 -0600

    fixed bug with crash if palce didnt have a picture

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,8 +1,10 @@
 class NotificationMailer < ActionMailer::Base
   default from: "no-reply@thelinksreport.com"
   
-  def comment_added
-    mail(to: "wcfirehose@gmail.com",
+  def comment_added(comment)
+    @place = comment.place
+    @place_owner = @place.user
+    mail(to: @place_owner.email,
       subject: "A comment has been added to your course!")
   end
 end
