366681127500c9e6459335f7d99d774e7d266c51
waders92 <wade.carlson@pga.com>
2016-12-01 12:07:24 -0600

    tested that email is sent when triggered

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -2,7 +2,7 @@ class NotificationMailer < ActionMailer::Base
   default from: "no-reply@thelinksreport.com"
   
   def comment_added
-    mail(to: "wade.carlson@pga.com",
+    mail(to: "wcfirehose@gmail.com",
       subject: "A comment has been added to your course!")
   end
 end
