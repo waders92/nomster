8db6bd921d1b5560c42c3926c48e18eaf741339e
waders92 <wade.carlson@pga.com>
2017-03-30 10:44:51 -0500

    fixed footer on login pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,28 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
-
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
-
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
-
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
-
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
- 
   root 'places#index'
-  
   resources :places do
-    resources :comments, :photos, only: :create
-   
+  resources :comments, :photos, only: :create
   end
-  
+
   resources :users, only: :show
   # Example resource route with options:
   #   resources :products do
